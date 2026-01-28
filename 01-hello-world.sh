eb65e6afc34cfb5af6eff45e99701f48e1ab0b68
Sriteja <nsriteja4u@gmail.com>
2026-01-28 18:25:23 -0500

    changes made in workspace

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -3,7 +3,7 @@
 #any line other than line 1 which start with # is a Comment
 echo "Hello World!"
 
-echo "if i make a change here in VsCode file following steps are to be followed:
+echo " if i make a change here in VsCode file following steps are to be followed:
 
 1. Save the file first - not like earlier Lol!
 2. In local (gitbash) we do the following Git add .(all files)  - Git commit - git push
