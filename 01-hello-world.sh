d618fc7da1689feef72e393b141bc972c1d09937
Sriteja <nsriteja4u@gmail.com>
2026-01-28 18:24:31 -0500

    changes made in workspace

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -2,3 +2,9 @@
 
 #any line other than line 1 which start with # is a Comment
 echo "Hello World!"
+
+echo "if i make a change here in VsCode file following steps are to be followed:
+
+1. Save the file first - not like earlier Lol!
+2. In local (gitbash) we do the following Git add .(all files)  - Git commit - git push
+3. In linux server we do: git pull nad run the file agaiun"
