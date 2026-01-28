4140a7a926aaa0afe60f114213bd86b82c35554c
Sriteja <nsriteja4u@gmail.com>
2026-01-28 18:25:57 -0500

    changes made in workspace

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -7,4 +7,4 @@ echo " if i make a change here in VsCode file following steps are to be followed
 
 1. Save the file first - not like earlier Lol!
 2. In local (gitbash) we do the following Git add .(all files)  - Git commit - git push
-3. In linux server we do: git pull nad run the file agaiun"
+3. In linux server we do: git pull nad run the file again"
