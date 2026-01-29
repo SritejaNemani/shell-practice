7a3ae3ddcdbeda2b17e3a853139dabccbdd8f867
Sriteja <nsriteja4u@gmail.com>
2026-01-28 19:45:23 -0500

    changes made in workspace

--- 07-variables.sh
+++ 07-variables.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 starttime=$(date)
+starttime_sec=$(date +%s)
 
-echo "Script startes at $starttime"
+echo "Script started at $starttime"
+echo "Script started at $starttime_sec seconds" #This is a numeric value
 
 sleep 10
 
 endtime=$(date)
+endtime_sec=$(date +%s)
 
 echo "Script end at $endtime"
+echo "Script end at $endtime_sec seconds" #This is a numeric value
+
+ total_time=($endtime_sec - $starttime_sec)
+echo "Total script rum time is $total_time"
