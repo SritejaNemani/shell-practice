e39763ba5d6fb3b15667be8959f13746344d011b
Sriteja <nsriteja4u@gmail.com>
2026-01-28 20:16:21 -0500

    changes made in workspace

--- 08-spl-variables.sh
+++ 08-spl-variables.sh
@@ -3,9 +3,9 @@
 echo "All args/variables passed to the script : $@"
 echo "Number of variables passes to script : $#"
 echo "Script Name : $@"
-echo "Present Working Directory : $pwd"
-echo "Who is running the script : $user"
-echo "Home directory of user : $home"
+echo "Present Working Directory : $PWD"
+echo "Who is running the script : $USER"
+echo "Home directory of user : $HOME"
 echo "PID of the script : $$"
 sleep 100&
 echo "PID of the recent background process is $!"
