10f0e03b4bc7fd46088b06af0a277190b35c4c44
Sriteja <nsriteja4u@gmail.com>
2026-01-28 19:26:19 -0500

    changes made in workspace

--- 05-variables.sh
+++ 05-variables.sh
@@ -5,3 +5,6 @@ read user_name
 echo "Username is $username"
 echo "Enter Password"
 read -s password
+echo "Password is hidden Enter secret code to display password"
+read -s secret_code
+echo " Password is $password"
