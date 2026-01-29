f5a18ef20cf9427133dc8a097002513ec1c15d66
Sriteja <nsriteja4u@gmail.com>
2026-01-28 19:47:15 -0500

    changes made in workspace

--- 07-variables.sh
+++ 07-variables.sh
@@ -14,5 +14,5 @@ endtime_sec=$(date +%s)
 echo "Script end at $endtime"
 echo "Script end at $endtime_sec seconds" #This is a numeric value
 
- total_time=(($endtime_sec - $starttime_sec))
+ total_time=$(($endtime_sec - $starttime_sec))
 echo "Total script rum time is $total_time"
