beedd868ce117e462a0410e1da17f6f958d08248
Sriteja <nsriteja4u@gmail.com>
2026-02-06 17:16:26 -0500

    shell practice

--- 11-installation.sh
+++ 11-installation.sh
@@ -4,6 +4,7 @@ USER_ID=$(id -u)
 
 if [ $USER_ID -ne 0 ]; then
    echo "Not root user - please run this script with root user"
+   exit 1 #if we dont give this Even if there is an error the script executes the next lines as well. In order to prevent this we exit with any failure code generally 1
 fi
 
 echo "Installing Nginx"
