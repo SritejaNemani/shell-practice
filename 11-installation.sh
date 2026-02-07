aa6e6befb28552532c5745d0d025ea69a4fae41c
Sriteja <nsriteja4u@gmail.com>
2026-02-06 20:57:49 -0500

    shell practice

--- 11-installation.sh
+++ 11-installation.sh
@@ -7,11 +7,22 @@ if [ $USER_ID -ne 0 ]; then
    exit 1 #if we dont give this Even if there is an error the script executes the next lines as well. In order to prevent this we exit with any failure code generally 1
 fi
 
-echo "Installing Nginx"
-dnf install nginxx -y
+VALIDATE(){
+   if [ $1 -ne 0 ]; then
+      echo "$2 Failure"
+   else 
+      echo "$2 Success"
+   fi
+}
 
-if [ $? -ne 0 ]; then
-   echo "Installing Nginx Failure"
-else 
-   echo "Installing Nginx Success"
-fi
+echo "Installing Nginx"
+dnf install nginx -y
+VALIDATE $? "Nginx Installation"
+
+echo "Installing NodeJs"
+dnf install nodejs -y
+VALIDATE $? "NodeJs Installation"
+
+echo "Installing MySQL"
+dnf install mysql -y
+VALIDATE $? "MySQL Installation"
