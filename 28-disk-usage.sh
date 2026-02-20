02f8e44fca2c2c30717ca08223679332c5b5fe5e
Sriteja <nsriteja4u@gmail.com>
2026-02-20 18:30:19 -0500

    shell-practice

---
+++ 28-disk-usage.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+R="\e[31m" #red
+G="\e[32m" #green
+Y="\e[33m" #yellow
+N="\e[0m" #normal-white
+
+
+log(){
+    echo -e "$(date "+%Y-%m %H:%M:%S") | $1" | tee -a $LOGS_FILE
+}
+
+DISK_USAGE=$(df -hT | grep -v Filesystem)
+USAGE_THRESHOLD=3
+
+while IFS= read -r line 
+do
+
+    USAGE=$(df -hT | grep -v Filesystem | awk '{print $6}' | cut -d "%" -f1)
+    PARTITION=$(df -hT | grep -v Filesystem | awk '{print $7}') 
+    if [ USAGEUSAGE -gt $USAGE_THRESHOLD]; then 
+        MESSAGE+="High Disk Usage on $PARTITION : $USAGE" #       if given Message= then everytime loop is executed message gets replaced - but here (+=) it appends the exixting      
+                  
+    fi
+
+done <<< $DISK_USAGE
+
+echo "$MESSAGE"
